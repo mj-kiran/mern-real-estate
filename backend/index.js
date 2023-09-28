00de4969a4902b947a5387707647d46f5e287431
mj-kiran <kiranmjoshua@gmail.com>
2023-09-28 13:07:02 +0530

    creatd a middleware and function to handle possible errors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,17 @@ const app = express();
 app.use(express.json())
 
 app.use('/api/user', userRouter);
-app.use('/api/auth',authRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'internal Server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
  
 app.listen(4001, () => {
     console.log('Server running on port 4001')
